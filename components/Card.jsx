b358516a2515be5fd7c0f9d0733eea476ea46736
hate-vtubers <gmolloflores@gmail.com>
2021-11-14 22:36:05 -0300

    change to 'priority' images

--- components/Card.jsx
+++ components/Card.jsx
@@ -6,7 +6,7 @@ export const Card = ({ title, imageArray }) => {
       <h4 className="text-center mb-2">{title}</h4>
       <picture className="flex items-center justify-evenly flex-wrap gap-3 md:gap-0 lg:gap-0 xl:gap-0 2xl:gap-0">
         {imageArray.map((i, index) => (
-          <Image src={i} alt={i} width={60} height={60} key={index} loading="lazy" />
+          <Image src={i} alt={i} width={60} height={60} key={index} priority />
         ))}
       </picture>
     </div>
